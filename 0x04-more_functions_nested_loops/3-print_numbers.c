4c96b7a09acb73cf49939fe951190b69e36beb75
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2023-03-23 01:16:20 -0700

    more functions and loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '1'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

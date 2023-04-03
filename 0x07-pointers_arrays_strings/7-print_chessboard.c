1a9ccc7167462c8ef13dcd9f104e972552c553d1
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2023-04-03 05:46:30 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,9 +6,13 @@
  * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
-{	int z;
+{	
+	int z;
+
 	for (z = 0; z < 8; ++z)
-	{	int j;
+	{	
+		int j;
+		
 		for (j = 0; j < 8; ++j)
 		{
 			printf("%c ", a[z][j]);
